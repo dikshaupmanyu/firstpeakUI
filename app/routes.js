dc719b423cd720cc78fa3c5bc9883d80fcc1da2d
diksha <diksha.novasoftcorps@gmail.com>
2021-03-18 22:43:39 -1000

    add csv part

--- app/routes.js
+++ app/routes.js
@@ -193,6 +193,59 @@ module.exports = function(app, passport) {
 
   });
 
+     app.get('/seanBlack_List_SMStatus', function (req, res , done) {
+   // console.log(res);
+    
+    var statusdata = req.query.status;
+    var timedata = req.query.currenttime;
+    // var tokenstatus = req.query.tokenval;        
+     var fs = require("fs");
+       let rawdata = fs.readFileSync('public/json/seanBlackListSM.json');
+       let userdetail = JSON.parse(rawdata);
+        // let userdetailNew = JSON.parse(userdetail);
+        // console.log(userdetail);  
+        // console.log(userdetail.status);
+        var userdetailt = { status : statusdata , currenttime : timedata};
+
+        let data = JSON.stringify(userdetailt, null, 2);
+
+        fs.writeFile('public/json/seanBlackListSM.json', data, (err) => {
+             if (err) throw err;
+             console.log(data);
+        });            
+        
+         // return res.redirect("/csvlist?tokendata="+tokenstatus);
+
+
+  });
+
+     app.get('/bushwickBlack_List_SMStatus', function (req, res , done) {
+   // console.log(res);
+    
+    var statusdata = req.query.status;
+    var timedata = req.query.currenttime;
+    // var tokenstatus = req.query.tokenval;        
+     var fs = require("fs");
+       let rawdata = fs.readFileSync('public/json/bushwickBlackListSM.json');
+       let userdetail = JSON.parse(rawdata);
+        // let userdetailNew = JSON.parse(userdetail);
+        // console.log(userdetail);  
+        // console.log(userdetail.status);
+        var userdetailt = { status : statusdata , currenttime : timedata};
+
+        let data = JSON.stringify(userdetailt, null, 2);
+
+        fs.writeFile('public/json/bushwickBlackListSM.json', data, (err) => {
+             if (err) throw err;
+             console.log(data);
+        });            
+        
+         // return res.redirect("/csvlist?tokendata="+tokenstatus);
+
+
+  });
+
+
 
 
   app.get('/Userimage', function (req, res , done) {
@@ -7357,6 +7410,45 @@ console.log(_id);
 
   });
 
+
+  
+
+       var fs =  require('fs');
+    var express = require('express');
+    var multer = require('multer');
+    var upload = multer({ dest: 'public/csv'});
+    app.post('/blacklist_SM-upload', function(req, res) {
+
+       console.log(req.files);
+
+
+      var file = 'public/csv/' + req.files.avatar.originalFilename;
+
+      var passUrl = req.body.dataUrl;
+       
+      console.log(passUrl);
+
+      // var checkdataval = req.body.checkdata;
+         console.log(req.files.avatar.path);
+
+      fs.rename(req.files.avatar.path, file, function(err) {
+        if (err) {
+          console.log(err);
+          res.send(500);
+        } else {
+
+             var string = encodeURIComponent('something that would break');
+
+
+              return res.redirect("/csvlist?tokendata="+passUrl+"&msg="+string);
+
+        
+
+        }
+
+      });
+
+  });
 
 
 
