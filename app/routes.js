d1b528a1ba2c430f21ede4d960eeb44d49b4957a
diksha <diksha.novasoftcorps@gmail.com>
2021-07-16 16:44:04 +0530

    add list

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,6 @@
 module.exports = function(app, passport) {
 
 // normal routes ===============================================================
-
   // show the home page (will also have our login links)
   app.get('/', function(req, res) {
     res.render('index.ejs');
